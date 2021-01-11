8d0a07c15d491a27ffbb624965f8e47b6b259705
Robert Graupner <robert.graupnerr@gmail.com>
2021-01-11 12:04:00 +0100

    add exercise 11.6

---
+++ Ex_11.6a/Ex_11.6a/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex_11._6a
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 20 - i; j > 0; j--)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ Ex_11.6b/Ex_11.6b/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex_11._6b
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+            while (i < 20)
+            {
+              int j = 20 - i;
+                while (j > 0)
+                {
+                    Console.Write(j + " ");
+                    j--;
+                }
+                Console.WriteLine("");
+                i++;
+            }
+        }
+    }
+}
